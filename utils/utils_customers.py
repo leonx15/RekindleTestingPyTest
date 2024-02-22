de82f1491a85cc26f1c3d84611021f30b42c6fde
Tomasz Leoniuk <tomasz.leoniuk@gmail.com>
2024-02-22 11:53:24 +0100

    Small adjustment

--- utils/utils_customers.py
+++ utils/utils_customers.py
@@ -12,10 +12,10 @@ def api_status_customers():
 
 
 def create_customer(json_data):
-        host = utils_main.load_config()["host_env"]
-        headers = {
-            "Authorization": f"Bearer {utils_main.get_jwt_token()}"
-        }
+    host = utils_main.load_config()["host_env"]
+    headers = {
+        "Authorization": f"Bearer {utils_main.get_jwt_token()}"
+    }
 
-        response = requests.post(f"http://{host}:8184/api/v1/customers", json=json_data, headers=headers)
-        return response
+    response = requests.post(f"http://{host}:8184/api/v1/customers", json=json_data, headers=headers)
+    return response
