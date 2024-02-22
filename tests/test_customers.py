6df84118e484a9fb445f72ffd76d195151435c65
Tomasz Leoniuk <tomasz.leoniuk@gmail.com>
2024-02-22 15:20:21 +0100

    Add comments

--- tests/test_customers.py
+++ tests/test_customers.py
@@ -4,13 +4,16 @@ import json
 import pytest
 
 schema_for_db = "customer.customers"
+
+
 @pytest.fixture()
 def user_data():
     return {
-            "username": "Test1",
-            "firstName": "Testiko",
-            "lastName": "Testowy"
-        }
+        "username": "Test1",
+        "firstName": "Testiko",
+        "lastName": "Testowy"
+    }
+
 
 class TestCustomers:
 
@@ -23,20 +26,22 @@ class TestCustomers:
         count_before = utils_main.count_items_in_db(schema_for_db)
         print(f"Count before: {count_before}")
 
-    # Step 1: Create User.
+        # Step 1: Create User.
         response, customer_id = utils_customers.create_customer(user_data)
-    # Step 2: Check the message and status code from endpoint.
+        # Step 2: Check the message and status code from endpoint.
         assert json.loads(response.text)["message"] == "Customer saved successfully!"
         assert response.status_code == 201
-    # Step 3: Check DB for created user.
+        # Step 3: Check DB for created user.
         count_after = utils_main.count_items_in_db(schema_for_db)
         print(f"Count after: {count_after}")
         assert count_after == count_before + 1
-    # Step 4: Remove created customer.
+        # Step 4: Remove created customer.
         utils_main.remove_items_in_db(schema_for_db, customer_id)
 
     def test_update_customer(self, user_data):
+        # Step 1: Create user.
         _, customer_id = utils_customers.create_customer(user_data)
+        # Step 2: Update values for customer.
         new_user_data = {
             "username": "UpTest1",
             "firstName": "UpTestiko",
@@ -44,17 +49,21 @@ class TestCustomers:
         }
         _, created_user_data = utils_customers.get_customer_data(customer_id)
         utils_customers.update_customer(customer_id, new_user_data)
+        # Step 3: Check updated values.
         _, updated_user_data = utils_customers.get_customer_data(customer_id)
         assert created_user_data != updated_user_data
-
+        # Step 4: Clean up after test.
         utils_main.remove_items_in_db(schema_for_db, customer_id)
 
     def test_delete_customer_by_api(self, user_data):
+        # Step 1: Create user.
         _, customer_id = utils_customers.create_customer(user_data)
         response, _ = utils_customers.get_customer_data(customer_id)
         assert response.status_code == 200
+        # Step 2: Delete created user.
         response = utils_customers.delete_customer(customer_id)
         assert response.status_code == 200
+        # Step 3: Check if user cannot be reach by API.
         try:
             response, _ = utils_customers.get_customer_data(customer_id)
             assert response.status_code == 400
