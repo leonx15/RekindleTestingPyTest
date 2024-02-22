46519e895ce6f696688f90c9235cfd68bc157dc6
Tomasz Leoniuk <tomasz.leoniuk@gmail.com>
2024-02-22 12:51:58 +0100

    Additional adjustments and function to handle delete and put in customers.

--- utils/utils_customers.py
+++ utils/utils_customers.py
@@ -2,22 +2,49 @@ from utils import utils_main
 import requests
 import json
 
-def api_status_customers():
-    host = utils_main.load_config()["host_env"]
-    headers = {
+host = utils_main.load_config()["host_env"]
+headers = {
         "Authorization": f"Bearer {utils_main.get_jwt_token()}"
     }
+
+
+def api_status_customers():
     response = requests.get(f"http://{host}:8184/api/v1/customers", headers=headers)
     return response
 
 
 def create_customer(json_data):
-    host = utils_main.load_config()["host_env"]
-    headers = {
-        "Authorization": f"Bearer {utils_main.get_jwt_token()}"
-    }
+    try:
+        response = requests.post(f"http://{host}:8184/api/v1/customers", json=json_data, headers=headers)
+        response.raise_for_status()
+        customer_id = json.loads(response.text)["customerId"]
+        print(f"User created: {customer_id}")
+        return response, customer_id
+    except requests.exceptions.HTTPError as http_err:
+        print(f"HTTP error occurred: {http_err}")
+    except Exception as err:
+        print(f"An error occurred: {err}")
 
-    response = requests.post(f"http://{host}:8184/api/v1/customers", json=json_data, headers=headers)
-    customer_id = json.loads(response.text)["customerId"]
-    print(f"User created: {customer_id}")
-    return response, customer_id
+
+def update_customer(customer_id, json_data):
+    try:
+        response = requests.put(f"http://{host}:8184/api/v1/customers/{customer_id}", json=json_data, headers=headers)
+        response.raise_for_status()
+        print(f"User updated: {customer_id}")
+        return response
+    except requests.exceptions.HTTPError as http_err:
+        print(f"HTTP error occurred: {http_err}")
+    except Exception as err:
+        print(f"An error occurred: {err}")
+
+
+
+def delete_customer(customer_id):
+    try:
+        response = requests.delete(f"http://{host}:8184/api/v1/customers/{customer_id}")
+        print(f"User {customer_id} successfully deleted")
+        return response
+    except requests.exceptions.HTTPError as http_err:
+        print(f"HTTP error occurred: {http_err}")
+    except Exception as err:
+        print(f"An error occurred: {err}")
