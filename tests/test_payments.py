e3ab24c4f61f4e38d810193ba9f3ad36414413dc
TLeoniuk <Tomasz.Leoniuk@gds.ey.com>
2024-02-22 08:59:55 +0100

    Test updates to the new structure.

--- tests/test_payments.py
+++ tests/test_payments.py
@@ -1,14 +1,13 @@
-from utils import get_jwt_token as jwt_token
-from utils import load_config
+from utils import utils_main
 import requests
 
 
 class TestPayments:
     def test_api_status(self):
-        host = load_config()["host_env"]
+        host = utils_main.load_config()["host_env"]
         order_id = "idOfOrder"
         headers = {
-            "Authorization": f"Bearer {jwt_token}"
+            "Authorization": f"Bearer {utils_main.get_jwt_token()}"
         }
         response = requests.get(f"http://{host}:8182/api/v1/payments/{order_id}", headers=headers)
         assert response.status_code == 200
