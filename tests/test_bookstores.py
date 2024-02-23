4a6e9cbd2b6f60149c2713c8a8c588340124ac23
Tomasz Leoniuk <tomasz.leoniuk@gmail.com>
2024-02-23 10:49:16 +0100

    Add tests for bookstores.

--- tests/test_bookstores.py
+++ tests/test_bookstores.py
@@ -1,21 +1,42 @@
 from utils import utils_main
+from utils import utils_bookstores
 import requests
+import pytest
+
+schema_for_db_bookstores = "bookstore.bookstores"
+
+
+@pytest.fixture()
+def bookstore_data():
+    bookstore_data = {
+        "name": "TestowaBooks",
+        "isActive": True
+    }
+    bookstore_update_data = {
+        "name": "UpdatedName",
+        "isActive": False
+    }
+    return bookstore_data, bookstore_update_data
 
 
 class TestBookstores:
     def test_api_status(self):
-        host = utils_main.load_config()["host_env"]
-        headers = {
-            "Authorization": f"Bearer {utils_main.get_jwt_token()}"
-        }
-        response = requests.get(f"http://{host}:8183/api/v1/bookstores", headers=headers)
+        response = utils_bookstores.get_list_of_bookstores()
         assert response.status_code == 200
 
-    def test_text_in_response(self):
-        host = utils_main.load_config()["host_env"]
-        headers = {
-            "Authorization": f"Bearer {utils_main.get_jwt_token()}"
-        }
-        response = requests.get(f"http://{host}:8183/api/v1/bookstores", headers=headers)
-        expected_string = "\"isActive\":false"
-        assert expected_string in response.text, "Expected text found."
+    def test_create_bookstore(self, bookstore_data):
+        response, bookstore_id = utils_bookstores.create_bookstore(bookstore_data[0])
+        assert response.status_code == 201
+        _, bookstore_data = utils_bookstores.get_specific_bookstore(bookstore_id)
+        assert bookstore_id == bookstore_data["id"]
+        utils_main.remove_items_in_db(schema_for_db_bookstores, bookstore_id)
+
+    def test_update_bookstore(self, bookstore_data):
+        _, bookstore_id = utils_bookstores.create_bookstore(bookstore_data[0])
+        response = utils_bookstores.update_bookstore(bookstore_id, bookstore_data[1])
+        assert response.status_code == 204
+        _, updated_bookstore_data = utils_bookstores.get_specific_bookstore(bookstore_id)
+        assert bookstore_id == updated_bookstore_data["id"]
+        assert updated_bookstore_data["name"] == bookstore_data[1]["name"]
+        assert updated_bookstore_data["isActive"] == bookstore_data[1]["isActive"]
+        utils_main.remove_items_in_db(schema_for_db_bookstores, bookstore_id)
--- utils/utils_bookstores.py
+++ utils/utils_bookstores.py
@@ -24,7 +24,7 @@ def create_bookstore(json_data):
     try:
         response = requests.post(f"http://{host}:8183/api/v1/bookstores", json=json_data, headers=headers)
         response.raise_for_status()
-        bookstore_id = response.text
+        bookstore_id = json.loads(response.text)
         return response, bookstore_id
     except requests.exceptions.HTTPError as http_err:
         raise AssertionError(f"HTTP error during creating bookstore: {http_err}")
@@ -34,7 +34,7 @@ def create_bookstore(json_data):
 
 def get_specific_bookstore(bookstore_id):
     try:
-        response = requests.get(f"http://{host}:8183/api/v1/bookstores", headers=headers)
+        response = requests.get(f"http://{host}:8183/api/v1/bookstores/{bookstore_id}", headers=headers)
         response.raise_for_status()
         bookstore_data = json.loads(response.text)
         return response, bookstore_data
@@ -42,3 +42,15 @@ def get_specific_bookstore(bookstore_id):
         raise AssertionError(f"HTTP error during looking for bookstore: {http_err}")
     except Exception as err:
         raise AssertionError(f"An error occurred: {err}")
+
+
+def update_bookstore(bookstore_id, json_data):
+    try:
+        response = requests.put(f"http://{host}:8183/api/v1/bookstores/{bookstore_id}", json=json_data, headers=headers)
+        response.raise_for_status()
+        return response
+    except requests.exceptions.HTTPError as http_err:
+        raise AssertionError(f"HTTP error during looking for bookstore: {http_err}")
+    except Exception as err:
+        raise AssertionError(f"An error occurred: {err}")
+
