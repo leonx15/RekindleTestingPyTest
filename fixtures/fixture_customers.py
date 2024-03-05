7cf6b840f14e5dc41ccef148a2f40ccb30a75ec5
TLeoniuk <Tomasz.Leoniuk@gds.ey.com>
2024-03-05 13:37:10 +0100

    replace user with customer

--- fixtures/fixture_customers.py
+++ fixtures/fixture_customers.py
@@ -1,31 +1,27 @@
 import pytest
 from utils import utils_main, utils_customers
 
-
-@pytest.fixture()
-def setup_schema_for_db():
-    schema_for_db = "customer.customers"
-    return schema_for_db
+schema_for_db_customers = "customer.customers"
 
 
 @pytest.fixture()
-def user_data():
-    new_user_data = {
+def customer_data():
+    new_customer_data = {
         "username": "Test1",
         "firstName": "Testiko",
         "lastName": "Testowy"
     }
-    updated_user_data = {
+    updated_customer_data = {
         "username": "UpdateTesto",
         "firstName": "UpdateFirst",
         "lastName": "UpdateLast"
     }
-    return new_user_data, updated_user_data
+    return new_customer_data, updated_customer_data
 
 
 @pytest.fixture()
-def create_new_customer(user_data, setup_schema_for_db):
-    new_user_data, _ = user_data
-    response, customer_id = utils_customers.create_customer(new_user_data)
+def create_new_customer(customer_data):
+    new_customer_data, _ = customer_data
+    response, customer_id = utils_customers.create_customer(new_customer_data)
     yield response, customer_id
-    utils_main.remove_items_in_db(setup_schema_for_db, customer_id)
+    utils_main.remove_items_in_db(schema_for_db_customers, customer_id)
--- tests/test_customers.py
+++ tests/test_customers.py
@@ -14,23 +14,23 @@ class TestCustomers:
         response, _ = utils_customers.get_customer_data(customer_id)
         assert response.status_code == 200
 
-    def test_update_customer(self, create_new_customer, user_data):
+    def test_update_customer(self, create_new_customer, customer_data):
         # Step 1: Create user.
         _, customer_id = create_new_customer
-        _, update_user_data = user_data
-        _, created_user_data = utils_customers.get_customer_data(customer_id)
-        utils_customers.update_customer(customer_id, update_user_data)
+        _, update_customer_data = customer_data
+        _, created_customer_data = utils_customers.get_customer_data(customer_id)
+        utils_customers.update_customer(customer_id, update_customer_data)
         # Step 2: Check updated values.
-        _, updated_user_data = utils_customers.get_customer_data(customer_id)
-        assert created_user_data["id"] == updated_user_data["id"]
-        assert updated_user_data["username"] == update_user_data["username"]
-        assert updated_user_data["firstName"] == update_user_data["firstName"]
-        assert updated_user_data["lastName"] == update_user_data["lastName"]
+        _, updated_customer_data = utils_customers.get_customer_data(customer_id)
+        assert created_customer_data["id"] == updated_customer_data["id"]
+        assert updated_customer_data["username"] == update_customer_data["username"]
+        assert updated_customer_data["firstName"] == update_customer_data["firstName"]
+        assert updated_customer_data["lastName"] == update_customer_data["lastName"]
 
 
-    def test_delete_customer_by_api(self, user_data):
+    def test_delete_customer_by_api(self, customer_data):
         # Step 1: Create user.
-        new_customer_data, _ = user_data
+        new_customer_data, _ = customer_data
         _, customer_id = utils_customers.create_customer(new_customer_data)
         response, _ = utils_customers.get_customer_data(customer_id)
         assert response.status_code == 200
