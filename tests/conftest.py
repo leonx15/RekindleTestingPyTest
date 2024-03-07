18c96cef7c2374086fe4d5ca9c180340c33aaddc
Tomasz Leoniuk <tomasz.leoniuk@gmail.com>
2024-03-07 08:27:00 +0100

    adjustments in code structure and add fixture_orders

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,2 +1,7 @@
 # In this file add new fixture directory if you will create any.
-pytest_plugins = ["fixtures.fixture_bookstores", "fixtures.fixture_customers", "fixtures.fixture_payments"]
+pytest_plugins = [
+    "fixtures.fixture_bookstores",
+    "fixtures.fixture_customers",
+    "fixtures.fixture_payments",
+    "fixtures.fixture_orders"
+]
