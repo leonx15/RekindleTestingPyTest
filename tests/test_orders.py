49daa2b7e01f4aa2f84c928af4549578ce8c9e63
TLeoniuk <Tomasz.Leoniuk@gds.ey.com>
2024-03-11 08:02:34 +0100

    minor adjustment

--- tests/test_orders.py
+++ tests/test_orders.py
@@ -4,5 +4,6 @@ class TestOrders:
 
     def test_create_purchase_order(self, set_up_orders):
         response, response_data = set_up_orders
+        assert response.status_code == 200
         assert response_data["message"] == "Order created successfully"
         print(f"Order data: {response_data} in {response}")
